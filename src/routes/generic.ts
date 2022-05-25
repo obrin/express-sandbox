301f3669be4be01397a21cbc4493db6a83615e1e
obrin <jordanyong23@gmail.com>
2022-05-25 12:42:19 -0400

    Add root routeS

--- src/routes/generic.ts
+++ src/routes/generic.ts
@@ -2,6 +2,10 @@ import * as express from 'express'
 
 const createRouter = (router: express.Router) => {
   router.get('/health', (_: express.Request, res: express.Response) => {
+    return res.send('healthy')
+  })
+
+  router.get('/', (_: express.Request, res: express.Response) => {
     return res.send('OK')
   })
 
